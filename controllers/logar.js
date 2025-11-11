1d458fab05acb1c74e4e721fdd6fbbf94312a09f
Thales <thalesbelle0@gmail.com>
2025-11-11 16:13:20 -0300

    upload 11/11

--- controllers/logar.js
+++ controllers/logar.js
@@ -7,15 +7,16 @@ module.exports = async (req, res, USERS) => {
   const { username, password } = req.body || {};
   
   if (!username || !password) {
+    console.log(username, password)
     return res.status(400).json({ error: 'username e password são necessários' });
   }
   
-  const user = USERS.find(u => u.username === username);
+  const user = USERS.find(u => u.usuario === username);
   if (!user) {
     return res.status(401).json({ error: 'Credenciais inválidas' });
   }
 
-  const senhaValida = await bcrypt.compare(password, user.password);
+  const senhaValida = await bcrypt.compare(password, user.senha);
   if(!senhaValida) return res.status(401).json({error: "Senha incorreta"});
 
   const payload = { id: user.id, username: user.username, name: user.name };
