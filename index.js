31df304117c6d14e1fa22baf2ca65b57c1deb327
vitor-valentin <vitor.rohling.becker@gmail.com>
2025-11-11 16:15:59 -0300

    Melhorado com try catch

--- index.js
+++ index.js
@@ -1,37 +1,48 @@
-require('dotenv').config();
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
 const bcrypt = require("bcryptjs");
-const logarController = require('./controllers/logar');
-const deslogarController = require('./controllers/deslogar');
-const verificaAutenticacao = require('./controllers/verificarAutenticacao');
-const util = require('util');
-const connection = require('./models/db');
+const logarController = require("./controllers/logar");
+const deslogarController = require("./controllers/deslogar");
+const verificaAutenticacao = require("./controllers/verificarAutenticacao");
+const util = require("util");
+const connection = require("./models/db");
 const query = util.promisify(connection.query).bind(connection);
 
 const app = express();
 
 async function getUsers() {
-    const res = await query("SELECT * FROM tbUsers");
-    let users = [];
-    res.forEach(element => {
-        users.push({id: element.idUser, username: element.username, password: element.passwordUser, name: element.nameUser});
-    });
-    return users;
+    try {
+        const res = await query("SELECT * FROM tbUsers");
+        let users = [];
+        res.forEach((element) => {
+            users.push({
+                id: element.idUser,
+                username: element.username,
+                password: element.passwordUser,
+                name: element.nameUser,
+            });
+        });
+        return users;
+    } catch (err) {
+        console.error("DEU ERRO NO MYSQL: ", err);
+    }
 }
 
-app.use(express.static(path.join(__dirname, 'src')));
+app.use(express.static(path.join(__dirname, "src")));
 app.use(express.json());
 app.use(cookieParser());
 
-app.post('/api/login', async (req, res) => logarController(req, res, await getUsers()));
-app.post('/api/logout', deslogarController);
+app.post("/api/login", async (req, res) =>
+    logarController(req, res, await getUsers())
+);
+app.post("/api/logout", deslogarController);
 
-app.get('/api/verifica', verificaAutenticacao, (req, res) => {
-  res.json({ message: 'Acesso permitido à rota protegida', user: req.user });
+app.get("/api/verifica", verificaAutenticacao, (req, res) => {
+    res.json({ message: "Acesso permitido à rota protegida", user: req.user });
 });
 
-app.listen(8080, () => {
-  console.log(`Servidor rodando em http://localhost:8080`);
+app.listen(8080, async () => {
+    console.log(`Servidor rodando em http://localhost:8080`);
 });
